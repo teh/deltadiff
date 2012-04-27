32c0cbff73046d626cb0de8cd30dcc527d9661d7
Thomas Hunger <tehunger@gmail.com>
2012-04-27 17:53:15 +0100

    Make pack/unpack functions private

--- deltadiff2.py
+++ deltadiff2.py
@@ -46,7 +46,7 @@ def generate_signature(path, block_size=BLOCK_SIZE):
             break
     return ''.join(signatures)
 
-def unpack_signature(binary_signature):
+def _unpack_signature(binary_signature):
     """
     Takes the binary encoded signature and returns 3-tuples
     of offset, token, hash
@@ -57,7 +57,7 @@ def unpack_signature(binary_signature):
 
 def generate_delta(path, signature, block_size=BLOCK_SIZE):
     sig_lookup = dict(
-        (token, hash) for _, token, hash in unpack_signature(signature)
+        (token, hash) for _, token, hash in _unpack_signature(signature)
     )
     data = open(path).read()
     i = 0
@@ -82,7 +82,7 @@ def generate_delta(path, signature, block_size=BLOCK_SIZE):
     deltas.append(('data', data[start:]))
     return deltas
 
-def pack_delta(delta):
+def _pack_delta(delta):
     binary_delta = []
     for what, data in delta:
         # length prefixed data or signature
@@ -92,7 +92,7 @@ def pack_delta(delta):
             binary_delta.append(struct.pack('>bI', BLOCK_USE, len(data)) + data)
     return ''.join(binary_delta)
 
-def unpack_delta(binary_delta):
+def _unpack_delta(binary_delta):
     i = 0
     while i < len(binary_delta):
         what, data_len = struct.unpack('>bI', binary_delta[i:i+5])
@@ -105,11 +105,11 @@ def unpack_delta(binary_delta):
         i += 5 + data_len
 
 def patch(in_path, out_path, signature, binary_delta, block_size=BLOCK_SIZE):
-    position_lookup = dict((hash, pos) for pos, _, hash in unpack_signature(signature))
+    position_lookup = dict((hash, pos) for pos, _, hash in _unpack_signature(signature))
 
     in_data = open(in_path).read()
     out_file = open(out_path, 'w')
-    for what, data_or_hash in unpack_delta(binary_delta):
+    for what, data_or_hash in _unpack_delta(binary_delta):
         if what == 'use':
             pos = position_lookup[data_or_hash]
             out_file.write(in_data[pos:pos + block_size])
@@ -138,7 +138,7 @@ def test_generate_delta():
             tf.flush()
         sig = generate_signature(tf.name)
         assert len(sig) == 252
-        delta = pack_delta(generate_delta(tf.name, sig))
+        delta = _pack_delta(generate_delta(tf.name, sig))
         assert len(delta) == 213
 
         # ensure we generate the same content
